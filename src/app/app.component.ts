e4ad3eb53ba0b5ff9356080a9d3c44dfced55e9e
koushikgopi <koushikgopikrishnan@gmail.com>
2019-11-15 18:33:59 +0530

    created a finder project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'find-anyone';
+
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
+  data = "";
+  response: any;
+  spinner = false;
+  err = false;
+
+  getUser() {
+
+    const localData = localStorage.getItem(this.data);
+
+    if (localData) {
+
+      this.err = false;
+      this.response = JSON.parse(localData);
+    }
+    else {
+
+      this.spinner = true;
+      console.log(this.data);
+      this.http.get('https://api.github.com/users/' + this.data).subscribe(userDetail => {
+        this.response = userDetail;
+        console.log(this.response);
+        localStorage.setItem(this.data, JSON.stringify(this.response));
+        this.spinner = false;
+        this.err = false;
+
+      },
+        err => {
+          this.spinner = false;
+          this.err = true;
+          this.response = false;
+          if (this.data) {
+            this.snackBar.open("incorrot Data", "ok", { duration: 3000, });
+ 
+          }
+          else
+          {   this.snackBar.open("No Data", "ok", { duration: 3000, });
+        }
+        });
+
+    }
+
+  }
+
 }
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
